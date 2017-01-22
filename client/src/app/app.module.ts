e95eb82f77b589b2f251d294a8a6469c7ec0de8e
ToniDias <toni.dias.91@gmail.com>
2017-01-22 20:03:56 +0100

    merge

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { QuestionComponent } from './question/question.component';
 import { GraphComponent } from './graph/graph.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import 'hammerjs';
+import { UserComponent } from './user/user.component';
+
 // other imports 
 @NgModule({
   declarations: [
@@ -28,7 +31,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     QuestionComponent,
     GraphComponent,
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserComponent
   ],
   imports: [
     ChartsModule,
